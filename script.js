d5f3a215cce2b85f49c0a5e7e28ae820c758962e
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-07-11 15:48:06 +0100

    Js datatype [string, number]

---
+++ script.js
@@ -0,0 +1,44 @@
+//window.alert('Hello, world!');
+/*
+let p = document.querySelector('p');
+p.innerHTML += '<a href="https://www.google.com"> Google</a> ';
+p.style.color = 'blue';
+*/
+/*
+document.write('<h2>Welcome to my page!</h2>');
+document.write('<p>This is a paragraph added by JavaScript.</p>');
+document.write('<p>Have a great day!</p>');
+*/
+
+// console.log('This is a message in the console.');
+
+//string output
+console.log("This is a message written in the console.");
+console.log("90 people");
+console.log(`$%/>`);
+
+//Number output
+console.log(100);
+console.log(2.14);
+console.log(-100);
+
+console.log(199 + 900);
+console.log(10 + (50 * 70) / 2);
+
+console.log(50 / 0);
+console.log(-50 / 0);
+console.log("james" - 50);
+console.log("" * 50);
+
+console.log(50 + "50" + ' eggs');
+console.log(10 + "20");
+
+console.log(0.1 + 0.2);
+console.log(20e4);
+
+//typeof operator=> used to check the type you are working with
+console.log(typeof 'hello');
+console.log(typeof 10);
+
+console.log(20);
+console.log('20');
