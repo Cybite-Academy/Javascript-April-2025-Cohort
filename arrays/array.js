d4635fc249189b7ceedb7690c5e337393f4c9fd0
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-07-29 15:18:11 +0100

    arrays and array destructuring

---
+++ arrays/array.js
@@ -0,0 +1,22 @@
+const fruits = ["orange", "apple", "banana", "kiwi"];
+const bucket = [1, 3, false, "hello", 2.5, "world", true, null, undefined];
+
+// console.log(fruits, bucket); //viewing the array
+
+// Accessing elements in an array
+// console.log(fruits[0]); // 'orange'
+// console.log(fruits[2]);
+// console.log(bucket[4]);
+
+
+// adding elements to an array
+const numbers = [];
+
+numbers[0] = 20;
+numbers[1] = 50;
+numbers[5]= 300
+
+// to overwrite an existing element
+numbers[1] = 100;
+console.log(numbers);
+
---
+++ conditionals/tenaryOperator.js
@@ -0,0 +1,24 @@
+const temp = 35;
+
+//if statement way
+// if (temp > 30) {
+//   console.log("It's a hot day");
+// } else {
+//   console.log("It is a cool day");
+// }
+
+//ternary operator
+`condition ? true : false`;
+
+// temp > 30 ?
+// console.log("it is a hot day") 
+// : console.log("it is a cool day");
+
+// classwork
+const number = 866;
+number % 2 === 0? console.log('number is even'): console.log('number is odd');
+
+
+const age = 16;
+const has_license = true;
+(age >= 16 && has_license)? console.log("You can drive") : console.log("You cannot drive");
